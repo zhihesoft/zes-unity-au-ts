e70c183c8f6835a4ad7d94a32a005e5efadbf925
stanley <stanley@zhihesoft.com>
2022-10-11 12:14:13 +0800

    try to use esm failed

--- Assets/Scripts/TestApp.cs
+++ Assets/Scripts/TestApp.cs
@@ -1,4 +1,6 @@
-﻿using Au.TS;
+﻿using Assets.Scripts;
+using Au.TS;
+using Puerts;
 using System;
 using System.IO;
 using UnityEngine;
@@ -6,22 +8,35 @@ using UnityEngine;
 public class TestApp : MonoBehaviour
 {
     TSApp app;
+    JsEnv env;
 
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
 
-        var script = File.ReadAllText("./Typescripts/out/main.bytes");
-        app = new TSApp("./Typescripts/dist/index.js");
-        app.Run();
-        //app.Run(script);
-        //var script = "./Typescripts/dist/index.js";
-        var fun = app.GetFunc<Action>("testFunc");
-        fun();
+        var loader = new TestLoader();
+        // env = new JsEnv(new DefaultLoader("Typescripts"));
+        env = new JsEnv(new TestLoader());
+        Puerts.ThirdParty.CommonJS.InjectSupportForCJS(env);
+        env.UsingFunc<int, string>();
+        var func = env.Eval<Func<int, string>>("var m = require('./Typescripts/dist/index.js'); m.i18n;");
+        // var func = env.ExecuteModule<Func<int, string>>("./Typescripts/dist/index.js", "i18n");
+        var test = func(100);
+        Debug.Log(test);
+
+
+        //// var script = File.ReadAllText("./Typescripts/out/main.bytes");
+        //app = new TSApp("./Typescripts/dist/index.js");
+        //app.Run();
+        ////app.Run(script);
+        ////var script = "./Typescripts/dist/index.js";
+        //var fun = app.GetFunc<Action>("testFunc");
+        //fun();
     }
 
     private void Update()
     {
-        app.Tick();
+        app?.Tick();
+        env?.Tick();
     }
 }
